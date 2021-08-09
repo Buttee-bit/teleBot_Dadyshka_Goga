26d362866dab32e359b8e18f2a791d9dfeab8219
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2021-08-09 23:45:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-import random
+from time import sleep
+
+import telebot
+from selenium import webdriver
+from telebot import types
 
 import Config
-import telebot
-from time import sleep
-from selenium import webdriver
-from telebot.types import Message
 
-driver = webdriver.Chrome()
 
 bot = telebot.TeleBot(Config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Запускаем Бота')
+
+@bot.message_handler(commands=['button'])
+def button(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    print(markup)
+    but_1 = types.InlineKeyboardButton('Будем искать видосы?',callback_data='find_vid_1')
+    but_2 = types.InlineKeyboardButton('Нам не нужны видосы?',callback_data='find_vid_0')
+    markup.add(but_1,but_2)
+
+    bot.send_message(message.chat.id, 'Это был ВАШ выбор',reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback(call):
+    if call.message:
+        if call.data == 'find_vid_1':
+            research_video(call.message)
+        else:
+            ask_fignia(call.message)
+            break
+
+
 
 @bot.message_handler(commands=['vind_video'])
 def research_video(message):
@@ -23,6 +43,7 @@ def research_video(message):
 def ask_fignia(message):
     bot.send_message(message.chat.id,'Я не ебу что ты от меня хочешь')
 def search(message):
+    driver = webdriver.Chrome()
     bot.send_message(message.chat.id,'Ща все будет')
     url_video = 'https://www.youtube.com/results?search_query=' + message.text + '&sp=EgIIAw%253D%253D'
     driver.get(url_video)
